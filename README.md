a51ea227558c145544d96799c49c9faa82d2d02d
Jong Wook Ryan Kim <diamon153@gmail.com>
2016-06-09 00:14:07 -0700

    completed the program

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# virtual_memory_manager Operating System Concepts Chapter 9 Project
+# Virtual Memory Manager Operating System Concepts Chapter 9 Project
+By Jong Wook Kim
+
+The program is in memory.c.
+To compile the program use "gcc memory.c"
+Then, to run the program use "./a.out INPUTFILE"
+For example, to run the program using address.txt as the input, run "./a.out address.txt"
--- memory.c
+++ memory.c
@@ -22,50 +22,58 @@ void page_miss(int logical_page, int offset);
 void page_hit(int logical_page, int offset);
 typedef enum {false, true} bool;
 
-signed char main_memory[MEMORY_SIZE];
-int page_table[PAGES];
-int tlb_page[TLB_SIZE];
-int tlb_loc[TLB_SIZE];
+signed char main_memory[MEMORY_SIZE]; // Contains data from BACKING_STORE.bin
+int page_table[PAGES]; // Array that maps logical page to physical page
+int tlb_page[TLB_SIZE]; // Array of logical pages stored in tlb
+int tlb_loc[TLB_SIZE]; // Corresponding physical pages
 signed char *backing; // Pointer to memory mapped backing file
 FILE * address_file;
-int logical_address;
-int tlb_replace;
-int total_count;
-int page_count;
-int tlb_hit;
+int logical_address; // Current logical address to access
+int tlb_replace; // Next tlb entry to replace using FIFO rule
+int total_count; // Total number of reads
+int page_count; // Total number of pages accessed
+int tlb_hit; 
 
 
 int main(int argc, const char *argv[]){
-	if (argc != 2) {
+	// Check for input validity
+    if (argc != 2) {
         printf("Invalid number of inputs. Input just the address txt file\n");
         exit(EXIT_FAILURE);
-    }
+    } 
 
+    // Get the input file and set-up
     address_file = fopen(argv[1], "r");
     initialize();
 
     // Read line by line and find the correct mapping
     while(!feof(address_file)) {
+        // Determine page # and offset
         int logical_page = (logical_address >> OFFSET_BITS) & PAGE_MASK;
         int offset = logical_address & OFFSET_MASK;
-        total_count++;
         bool tlb_miss = true;
+        
+        // Check if the logical_page is in tlb
         for (int i = 0; i < TLB_SIZE; i++) {
+            // If there is a tlb_hit, get the data and print the output
             if (tlb_page[i] == logical_page) {
                 int physical_address = (tlb_loc[i] << OFFSET_BITS) | offset;
                 signed char value = main_memory[physical_address];
                 printf("V_add:%5d(%4d,%4d)   P_addr:%5d(%4d,%4d) Value: %8d\n", logical_address, logical_address/256, logical_address%256, physical_address,  physical_address/256, physical_address%256, value);
+                
                 tlb_miss = false;
                 tlb_hit++;
             }
         }
 
-        // See if the logical_page exist 
+        // If we have tlb miss, check if we have page fault
         if ((page_table[logical_page] == -1) && tlb_miss)
-            page_miss(logical_page, offset);
+            page_miss(logical_page, offset); // page fault
         else if (tlb_miss)
-            page_hit(logical_page, offset);
+            page_hit(logical_page, offset); // page hit
 
+        // Increment the counter and read the next line
+        total_count++;
         fscanf(address_file, "%d", &logical_address);
     }
 
@@ -74,7 +82,7 @@ int main(int argc, const char *argv[]){
 }
 
 void initialize() {
-    // Get BACKING_STORE data
+    // Get BACKING_STORE data and map the data to backing 
     int backing_fd = open("BACKING_STORE.bin", O_RDONLY);
 	backing = mmap(0, MEMORY_SIZE, PROT_READ, MAP_PRIVATE, backing_fd, 0); 
 
@@ -86,7 +94,7 @@ void initialize() {
     for (int i = 0; i < TLB_SIZE; i++)
         tlb_page[i] = -1;
 
-    // Open address txt
+    // Open address txt and read the file line into logical_address
     if (address_file == NULL) {
         printf("Invalid file\n");
         exit(EXIT_FAILURE);
@@ -101,6 +109,7 @@ void initialize() {
 }
 
 void end_program() {
+    // Output the relevant data and close file
     printf("Number of Translated Addresses = %d\n", total_count);
     printf("Page Faults = %d\n", page_count);
     printf("Page Fault Rate = %.3f\n", (double)page_count / total_count);
@@ -115,6 +124,7 @@ void page_miss(int logical_page, int offset) {
     int physical_address = (page_count << OFFSET_BITS) | offset;
     signed char value = main_memory[physical_address];
     
+    // update page_table
     page_table[logical_page] = page_count;
 
     // tlb update
@@ -128,6 +138,7 @@ void page_miss(int logical_page, int offset) {
 }
 
 void page_hit(int logical_page, int offset) {
+    // If there is page hit get the relevant data from page_table
     int physical_address = (page_table[logical_page] << OFFSET_BITS) | offset;
     signed char value = main_memory[physical_address];
    
